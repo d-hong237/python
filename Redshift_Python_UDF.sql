4d50a85ce3d7fd2a51cd4f7c80c5625e2fad4d8c
Danny <daniel.hong0237@gmail.com>
2017-11-29 19:04:08 -0800

    Update Redshift_Python_UDF.sql

--- Redshift_Python_UDF.sql
+++ Redshift_Python_UDF.sql
@@ -1,51 +1,67 @@
-create or replace function udf_weight_std_dev(observation varchar(2000), freq varchar(2000))
+create or replace function udf_weight_std_dev(observation varchar(3000), freq varchar(3000))
 returns float immutable as $$
 
 '''
+Change Log:
+11/15/17 - Allowed observation variable to be either an int or float.
+         - Implemented constraint on observation to only have up to 3 decimal places.
+11/29/17 - Updated example on documentation below.
+
+
 Created on 11/6/2017 
 UDF function calculates weighted standard deviation(population)
 between the observation and freq variables.
+
+Observation can be either a float or int.
+Freq must be an int.
 
 Data prep needs to be performed before using this function as the variables will be passed in as a string.
 
 Check these steps before using this UDF.
 1. Get distinct values from observation variable, sum the freq variable 
-and group it by the observation variable and any other dimension fields as needed. 
+and group it by the any dimension fields as needed. 
 
 2. Transpose observation and freq variable to a single row using Redshift LISTAGG() function. 
 
 See example below:  
 
-create temp table sample
-as
-select b.reporting_Week_Begin_Date, a.code,
-cast(a.<observation> as int) as observation, sum(a.<freq>) as freq
-from table_a a
-join table_b b
-on a.a_day = b.b_day
-and a.<freq>  > 0
-group by 1, 2, 3
-order by 2;
+vendor_code  obs      freq
+ABC           5         3
+ABC           6         5
+ABC           7         1
+
+
+Observation: [5,6,7]  
+Freq: [3,5,1]
+
+Weighted Standard Deviation = [5,5,5,6,6,6,6,6,7] 
  
- 
-create temp table get_results
-sortkey(reporting_week_Begin_date, vendor_code)
-as
+
  
 select
-reporting_week_Begin_Date,
-code,
-udf_weight_std_dev( LISTAGG(observation, ','), LISTAGG(freq,',') ) as weighted_std_dev
+vendor_code,
+udf_weight_std_dev( LISTAGG(tilt, ','), LISTAGG(quantity_unpacked,',') ) as weighted_std_dev
 from sample
-group by 1, 2
-order by 2;
+group by 1;
 '''
 
 import numpy as np
 
 def getWeightstdev(observation, freq):
 
-   observation = [int(i) for i in observation.split(',')]
+   #Check to see if string contains a dot. If so, check decimal places.
+   if '.' in observation:
+      observation = [float(i) for i in observation.split(',')]
+   
+      for x in observation:
+         if len(str(x).split('.')[1]) > 3:
+            raise ValueError("Observation variable can only have up to 3 decimal places")
+         else:
+            pass
+   else:
+      observation = [float(i) for i in observation.split(',')]
+
+
    freq = [int(i) for i in freq.split(',')]
 
    n = 0
